696c9fafed73e9b11cabb54c2f4f8730c4da2c2b
NIGHTWALKEROFC <saraamuhammad3@gmail.com>
2026-08-24 15:21:35 +0530

    Create config.py

---
+++ windows-app/config.py
@@ -0,0 +1,11 @@
+SERVER_URL = "http://localhost:8420"
+SESSION_ID = "windows-main"
+
+WAKE_PHRASE = "hey nox"
+SLEEP_PHRASE = "nox sleep"
+
+SAMPLE_RATE = 16000
+PASSIVE_CHUNK_SECONDS = 3
+COMMAND_CHUNK_SECONDS = 5
+
+WHISPER_MODEL_SIZE = "base"
